17c262fd1480ffbbe48ca12f6b3c59234e725538
grocid <carl@grocid.net>
2016-11-02 18:12:48 +0100

    rsa

--- rsa.py
+++ rsa.py
@@ -1,16 +1,27 @@
-import math, requests, re, sys
+import gmpy2, requests, re, sys
+
+def sieve(n):
+    """ http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188 """
+    n, correction = n-n%6+6, 2-(n%6>1)
+    sieve = [True] * (n/3)
+    for i in xrange(1,int(n**0.5)/3+1):
+      if sieve[i]:
+        k=3*i+1|1
+        sieve[      k*k/3      ::2*k] = [False] * ((n/6-k*k/6-1)/k+1)
+        sieve[k*(k-2*(i&1)+4)/3::2*k] = [False] * ((n/6-k*(k-2*(i&1)+4)/6-1)/k+1)
+    return [2,3] + [3*i+1|1 for i in xrange(1,n/3-correction) if sieve[i]]
 
 def check_sqrt(n, p):
-	q = int(math.sqrt(n))
+	q = int(gmpy2.iroot(n, 2)[0])
 	for i in range(q, q + p):
-		if n % q == 0:
-			return q, n / q
+		if n % i == 0:
+			return i, n / i
 	return None, None
 	
 def find_small_factor(n, p):
-	for i in range(0, p):
-		if n % p == 0:
-			return p, n / p
+	for i in p:
+		if n % i == 0:
+			return i, n / i
 	return None, None
     
 def check_factordb(n):
@@ -24,7 +35,7 @@ if len(sys.argv) > 1:
 else:
     sys.exit()
 
-print 'Prime: {0}'.format(n)
+print 'Number (n): {0}'.format(n)
 print '[ ] Checking factor.db'
 
 if check_factordb(n) != 0:
@@ -34,18 +45,21 @@ if check_factordb(n) != 0:
 else: 
     print '[-] No factors found...'
 
-print '[ ] Checking small factor (delta = 1e6)'
-p, q = find_small_factor(n, int(1e6))
+print '[ ] Checking small factor (delta = 1e8)'
+primes = sieve(int(1e8))
+p, q = find_small_factor(n, primes)
     
 if p != None:
     print 'N = {0} * {1}'.format(p, q)
     sys.exit()
 else: print '[-] No factors found...'
     
-print '[ ] Checking factor close to sqrt(n) (delta = 1e6)'
-p, q = check_sqrt(n, int(1e6))
+print '[ ] Checking factor close to sqrt(n) (delta = 1e8)'
+p, q = check_sqrt(n, int(1e8))
     
 if p != None:
     print 'N = {0} * {1}'.format(p, q)
     sys.exit()
 else: print '[-] No factors found...'
+
+print int(gmpy2.iroot(n, 2)[0])
